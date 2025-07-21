44cc006cd2f79e182b483f094257d4dbe76f91e9
Abdur Rehman <malik.a.rehman04@gmail.com>
2025-07-21 11:55:22 -0700

    chore: small fixes

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,5 +1,5 @@
 export default async function PostDetail({params}){
-    const {id} = params
+    const {id} = await params
 
     const res =await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
     const obj = await res.json();
